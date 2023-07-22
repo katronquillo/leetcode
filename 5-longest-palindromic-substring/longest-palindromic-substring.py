9ff53da442eed0e4f55cc7ce2c493bed78a465e7
Katrina Ronquillo <56237953+katronquillo@users.noreply.github.com>
2023-07-22 17:20:26 -0400

    Time: 725 ms (53.08%) | Memory: 16.3 MB (95.27%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,31 @@
+class Solution:
+    """
+    babad
+    """
+    def longestPalindrome(self, s: str) -> str:
+        palindrome, longestLength = "", 0
+
+        # Find the longest palindrome with the current char as "middle"
+        for i in range(len(s)):
+            # Looking for odd length palindromes (I.E. Mirrored excluding curr char)
+            left, right = i, i
+            while (0 <= left and right < len(s) and s[left] == s[right]):
+                currLength = (right - left) + 1
+                if (currLength > longestLength):
+                    longestLength = currLength
+                    palindrome = s[left:right + 1]
+                left -= 1
+                right += 1
+            
+            # Looking for even length palindromes (I.E. Mirror including curr char)
+            left, right = i, i + 1
+            while (0 <= left and right < len(s) and s[left] == s[right]):
+                currLength = (right - left) + 1
+                if (currLength > longestLength):
+                    longestLength = currLength
+                    palindrome = s[left:right + 1]
+                left -= 1
+                right += 1
+
+        return palindrome
+        
